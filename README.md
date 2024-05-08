6a6c3ab895281974c1eb08666684a7ebb53192a9
Muhammad Zuhair Zuhdi <joe@frostfire.id>
2024-05-09 00:38:57 +0700

    feat: readme and index.php on root

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# TUGAS MODUL PHP 1 - 9 X RPL
+
+
+
+
+## Authors
+
+- [MUHAMMAD ZUHAIR ZUHDI  |  X RPL 6](https://frostfire.id)
+
+
+## Demo
+
+http://tugasphpjoe.free.nf/
+
+
+## Features
+
+- Make confirmation transaction sistem betwen user and admin / SuperAdmin
+- increase & decrease product quantity on cart
+- User Admin Dashboard
+- History transaction
+- CRUD product
+- CRUD User (Admin & SuperAdmin)
+- CRUD Admin (SuperAdmin)
+- Save Cart and transaction on Database
+
+
+## Feedback
+
+If you have any feedback, please reach out to us at joe@frostfire.id
+
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Menu dan Deskripsi</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <style>
+    /* Custom styles jika diperlukan */
+  </style>
+</head>
+<body>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 text-center">
+        <h1>Halooo!</h1>
+        <p>Pilih halaman yang ingin Anda kunjung</p>
+        <div class="mt-4">
+          <a href="./materi/login.php" class="btn btn-primary mr-3">Materi</a>
+          <a href="./tugas/login.php" class="btn btn-secondary">Praktikum</a>
+        </div>
+        <div class="mt-5">
+          <h2>Deskripsi</h2>
+          <p class="">
+            Tugas modul 1 - 9
+            Muhammad Zuhair Zuhdi X RPL 6
+          </p>
+          <p class=""> Password login</p>
+          <p class=""> Praktikum SuperAdmin : SuperAdmin</p>
+          <p class=""> Password SuperAdmin  : 12345</p>
+          <p class=""> Praktikum Admin      : Admin</p>
+          <p class=""> Password Admin       : 12345</p>
+          <p class=""> Praktikum User1      : user</p>
+          <p class=""> Password User1       : 1234</p>
+          <p class=""> Praktikum User2      : gabut</p>
+          <p class=""> Password User2       : 1234</p>
+          <p class=""> Materi Akun          : gabut</p>
+          <p class=""> Password Akun        : </p>
+          <p class=""> 1234</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
